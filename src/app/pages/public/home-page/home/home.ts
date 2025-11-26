7554153bb076d4526ea3079ac22eba211fb4c077
Victor Izquierdo PC <vizquierdop@uoc.edu>
2025-11-27 00:12:11 +0100

    #016 - Public home page (Checkpoint)

---
+++ src/app/pages/public/home-page/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class PublicHome {
+
+}

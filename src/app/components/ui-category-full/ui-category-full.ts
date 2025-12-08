c525a2c0a8cd7f09e783164ed53fac02261a641a
Victor Izquierdo PC <vizquierdop@uoc.edu>
2025-12-09 00:52:26 +0100

    #027 - Load profile data in Admin Module

--- src/app/components/ui-category-full/ui-category-full.ts
+++ src/app/components/ui-category-full/ui-category-full.ts
@@ -1,17 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CategoryModel } from '../../models/entities/category.models';
+import { BusinessCategoriesService } from '../../services/business-categories.service';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ConfirmationModal } from '../confirmation-modal/confirmation-modal';
+import { EMPTY, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-ui-category-full',
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './ui-category-full.html',
   styleUrl: './ui-category-full.scss',
 })
 export class UiCategoryFull {
   @Input() category!: CategoryModel;
+  @Output() hasToReload$ = new EventEmitter<void>();
 
-  constructor() {}
+  constructor(
+    private readonly businessCategoriesService: BusinessCategoriesService,
+    private readonly dialog: MatDialog
+  ) {}
+
+  openConfirmationModal(): void {
+    const dialogRef = this.dialog.open(ConfirmationModal, {
+      data: {
+        message:
+          'The relation between this category and your business will be removed. Do you want to continue?',
+      },
+    });
+
+    let hasToReload = false;
+
+    dialogRef.afterClosed().pipe(
+      switchMap(result => {
+        // if (result) {
+        //   hasToReload = true;
+        //   return this.businessCategoriesService.delete()
+        // }
+        return EMPTY;
+      })
+    ).subscribe(() => {
+      if (hasToReload) {
+        this.hasToReload$.emit();
+      }
+    });
+  }
 }
--- src/app/pages/admin/profile-page/modals/add-category-modal/add-category-modal.ts
+++ src/app/pages/admin/profile-page/modals/add-category-modal/add-category-modal.ts
@@ -1,13 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, Inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { UISelectModel } from '../../../../../models/basic/ui-select.model';
+import { UsersService } from '../../../../../services/users.service';
+import { BusinessCategoriesService } from '../../../../../services/business-categories.service';
+import { BusinessCategoryCreateRequestDTO } from '../../../../../models/dtos/business-category.dto.models';
+import { catchError, EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-add-category-modal',
@@ -31,18 +35,21 @@ export class AddCategoryModal {
   categoryOptions: UISelectModel[] = [];
 
   constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      categories: UISelectModel[];
+      businessId: number;
+    },
     private readonly dialog: MatDialogRef<AddCategoryModal>,
-    private readonly fb: FormBuilder
+    private readonly fb: FormBuilder,
+    private readonly businessCategoriesService: BusinessCategoriesService
   ) {
     this.addCategoryForm = this.fb.group({
+      businessId: [null, Validators.required],
       categoryId: [null, [Validators.required]],
     });
-
-    this.loadSelectValues();
-  }
-
-  loadSelectValues(): void {
-    // TODO Implement loadSelectValues method.
+    this.categoryOptions = this.data.categories;
+    this.addCategoryForm.get('businessId')?.setValue(this.data.businessId);
   }
 
   close(): void {
@@ -50,6 +57,18 @@ export class AddCategoryModal {
   }
 
   save(): void {
-    // TODO Implement save method.
+    const request: BusinessCategoryCreateRequestDTO = {
+      businessId: this.addCategoryForm.get('businessId')?.value,
+      categoryId: this.addCategoryForm.get('categoryId')?.value,
+    }
+    this.businessCategoriesService.create(request).pipe(
+      catchError(() => {
+        this.isLoading.set(false);
+        return EMPTY;
+      })
+    ).subscribe(() => {
+      this.isLoading.set(false);
+      this.dialog.close(true);
+    });
   }
 }
